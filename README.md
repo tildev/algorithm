c34d36391faccfa7a49a3a61220c1d6c2f141ff3
tildev <tildevelove@gmail.com>
2022-04-09 19:27:50 +0900

    Solve the problem 2557 of b-s01

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # algorithm
+
+## baekjoon
+- step01
+    - 2557 : Hello World [x]
+    - 
---
+++ src/main/java/baekjoon/s01/p2557/Main.java
@@ -0,0 +1,7 @@
+package baekjoon.s01.p2557;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
