6810aa12e993b5c80f1fa1cf9eefaf3580860962
tildev <tildevelove@gmail.com>
2022-04-09 19:52:30 +0900

    Solve the problem 10172 of b-s01

---
+++ src/main/java/baekjoon/s01/p10172/Main.java
@@ -0,0 +1,15 @@
+package baekjoon.s01.p10172;
+
+/**
+ * @author : tildev
+ * @data : 2022-04-09
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+    }
+}
