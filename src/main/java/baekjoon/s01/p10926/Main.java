33f068d60efcdc899bc462106647034a1316c8c9
tildev <tildevelove@gmail.com>
2022-04-10 21:37:24 +0900

    Solve the problem 10926 of b-s01

---
+++ src/main/java/baekjoon/s01/p10926/Main.java
@@ -0,0 +1,17 @@
+package baekjoon.s01.p10926;
+
+import java.io.*;
+
+/**
+ * @author : tildev
+ * @data : 2022-04-10
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(br.readLine() + "??!");
+        bw.close();
+    }
+}
