11ba5559e2936ad52b439f300b749255cf7e899c
tildev <tildevelove@gmail.com>
2022-04-11 10:00:59 +0900

    Solve the problem 1330 of b-s02

---
+++ src/main/java/baekjoon/s01/p1330/Main.java
@@ -0,0 +1,30 @@
+package baekjoon.s01.p1330;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * @author : tildev
+ * @data : 2022-04-11
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(s, " ");
+        int a = Integer.valueOf(st.nextToken());
+        int b = Integer.valueOf(st.nextToken());
+
+        if (a > b) {
+            bw.write(">");
+        } else if (a < b) {
+            bw.write("<");
+        } else {
+            bw.write("==");
+        }
+        bw.close();
+    }
+}
