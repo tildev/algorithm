de7635353cf3bb9e0a4cccc7d566dc6716515877
tildev <tildevelove@gmail.com>
2022-04-14 20:36:22 +0900

    Solve the problem 8393 of b-s03

---
+++ src/main/java/baekjoon/s03/p8393/Main.java
@@ -0,0 +1,23 @@
+package baekjoon.s03.p8393;
+
+import java.io.*;
+
+/**
+ * @author : tildev
+ * @data : 2022-04-14
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.valueOf(br.readLine());
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        bw.write(sum + "");
+        bw.close();
+    }
+}
