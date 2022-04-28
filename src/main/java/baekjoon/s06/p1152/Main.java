11965328c78d772b206c547c9528420b530e4623
tildev <tildevelove@gmail.com>
2022-04-29 07:40:29 +0900

    Solve the problem 1152 of b-s06

---
+++ src/main/java/baekjoon/s06/p1152/Main.java
@@ -0,0 +1,19 @@
+package baekjoon.s06.p1152;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+**
+* @author : tildev
+* @data : 2022-04-29
+*/
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        System.out.println(st.countTokens());
+    }
+}
