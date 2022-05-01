241572f93ab65b93b4d5d3e01bd97573a0abd489
tildev <tildevelove@gmail.com>
2022-05-01 19:23:13 +0900

    Solve the problem 25083

---
+++ src/main/java/baekjoon/s01/p25083/Main.java
@@ -0,0 +1,16 @@
+package baekjoon.s01.p25083;
+
+/**
+ * @author : tildev
+ * @data : 2022-05-01
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
